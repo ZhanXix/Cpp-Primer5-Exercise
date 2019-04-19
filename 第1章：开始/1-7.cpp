e67f5911d6e541e4d554de33dfe889a313dcdd2d
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-04-19 10:04:49 +0800

    Create 1-7.cpp

---
+++ 第1章：开始/1-7.cpp
@@ -0,0 +1,9 @@
+int main()
+{
+	/***************** 
+	/*这是一个嵌套注释*/ 
+	******************/
+	return 0;          //无法正确编译 
+}
+//编译器返回的错误信息为： [Error] expected primary-expression before '/' token
+//                         [Error] expected primary-expression before 'return'
