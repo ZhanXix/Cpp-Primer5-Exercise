25253453d96275de3646984bf56d39ddf4b7eef7
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-04-19 10:09:02 +0800

    Create 1-13-3.cpp

---
+++ 第1章：开始/1-13-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int val=0,min=0,max=0;
+	cout<<"输入两个整数："<<endl;
+	cin>>min>>max;
+	if(min>max){	
+	//如果输入的数不是第一个小，第二个大
+	//就需要对换 
+		int temp=min;
+		min=max;
+		max=temp;
+	} 
+	for(val=min;val<=max;++val)
+		cout << val << endl; 
+	//输出min到max之间的所有整数 
+	return 0;          
+}
