24bf41110b268d54cae2c8688fca93452c93c98f
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-04-19 10:03:52 +0800

    Create 1-4.cpp

---
+++ 第1章：开始/1-4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"输入两个数字："<<endl;
+	int v1=0,v2=0;
+	cin>>v1>>v2;
+	cout<<v1<<"和"<<v2<<"的乘积是"<<v1*v2<<endl;  //输出两个数的积
+	return 0; 
+}
