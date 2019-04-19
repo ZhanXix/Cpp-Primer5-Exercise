41f475a6a982ba7381acfeb9220b8120f78bd65d
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-04-19 10:08:27 +0800

    Create 1-13-2.cpp

---
+++ 第1章：开始/1-13-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//只要val的值不小于0，for循环就会持续执行
+	for (int val=10;val>=0;--val)
+		cout << val << endl; 
+	//输出10到0之间的所有整数 
+	return 0;          
+}
