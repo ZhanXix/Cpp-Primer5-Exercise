731a29ea5b947eec6d372994c801568d1434faa6
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-04-29 22:41:13 +0800

    Create 2-40.cpp

---
+++ 第2章：变量和基本类型/2-40.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+struct Sales_data{
+	string ISBN;	//ISBN号
+	int count=0;	//售出的册数 
+	double money=0;	//总销售额 
+	double avr=0; 	//平均价格 
+}; 
+int main(){
+	return 0;
+} 
