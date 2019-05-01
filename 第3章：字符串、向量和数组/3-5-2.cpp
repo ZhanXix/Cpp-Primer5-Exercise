cc9ee2911035c0d0de2da549981f91858d283801
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-05-01 11:55:47 +0800

    Create 3-5-2.cpp

---
+++ 第3章：字符串、向量和数组/3-5-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+int main(){
+	cout << "输入多个字符串：" << endl;
+	string s,big_s;
+	while(cin >> s){	//读入多个字符串 
+		big_s += s + " ";		//将它们连接在一起（用空格分割） 
+	}
+	cout << big_s << endl;//输出连接成的大字符串 
+	return 0;
+}
