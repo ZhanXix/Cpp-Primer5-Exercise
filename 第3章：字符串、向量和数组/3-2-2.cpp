84735796be0673a5802d6eb42927a67ad578c160
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-05-01 11:33:56 +0800

    Create 3-2-2.cpp

---
+++ 第3章：字符串、向量和数组/3-2-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+int main(){
+	string word;
+	cin >> word;	//一次读入一个词 
+	cout << word << endl;
+	return 0;          
+}
