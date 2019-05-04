3bf720428c80c46eda5ae1cce9ed51da15272c24
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-05-04 22:27:04 +0800

    Create 3-44.cpp

---
+++ 第3章：字符串、向量和数组/3-44.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using int4 = int[4];
+int main(){
+	//输出ia的所有元素 
+	//使用类型别名 
+	int ia[3][4] = {0,1,2,3,4,5,6,7,8,9,10,11};
+	//版本一：范围for语句
+	cout << "版本一：" << endl;
+	for(int4 &p : ia)
+		for(int q : p)
+			cout << q << " ";
+	cout << endl;
+	//版本二：下标运算符 
+	cout << "版本二：" << endl;
+	for(int p=0; p!=3; ++p)
+		for(int q=0; q!=4; ++q)
+			cout << ia[p][q] << " ";
+	cout << endl;
+	//版本三：指针 
+	cout << "版本三：" << endl;
+	for(int4 *p=ia; p!=ia+3; ++p)
+		for(int *q=*p; q!=*p+4; ++q)
+			cout << *q << " ";
+	cout << endl;
+	return 0; 
+}
+
