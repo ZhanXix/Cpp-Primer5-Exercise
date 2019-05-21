aa27dc4c355472e93e9b648fa7fd343a520e58ef
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-05-21 22:59:16 +0800

    Create 5-4.cpp

---
+++ 第5章：语句/5-4.cpp
@@ -0,0 +1,12 @@
+//可读性降低了
+#include <iostream>
+using namespace std;
+int main() {
+	int sum = 0, val = 1;
+	//只要val的值小于等于10，while循环就会持续执行
+	while (val <= 10)
+		sum += val, ++val;
+	std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+	return 0;
+}
+
