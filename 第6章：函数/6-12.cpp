73df196c58c8e4c4e7e4a555dabff9fb7b4b1536
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-05-26 12:44:02 +0800

    Create 6-12.cpp

---
+++ 第6章：函数/6-12.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void exchange(int &v1, int &v2) {
+	//交换v1和v2的值
+	int temp = v1;
+	v1 = v2;
+	v2 = temp;
+}
+
+int main()
+{
+	cout << "输入两个数字：" << endl;
+	int x, y;
+	cin >> x >> y;
+	exchange(x, y);
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+	return 0;
+}
+//我觉得引用更易于使用，简单方便
